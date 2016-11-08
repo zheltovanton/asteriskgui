5878018d62942d82146b09dacb431b22e5a81feb
zheltovanton <anton.zheltov@gmail.com>
2016-11-08 22:56:03 +0300

    add config

---
+++ db/config.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+    "db" => "mysql:host=localhost;dbname=asteriskcdrdb",
+    "username" => "root",     //Mysql login
+    "password" => "password", //Mysql password
+    "options" => array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'),
+    "asterisk_ip" => "127.0.0.1",
+    "manager_port" => "5038",
+    "manager_login" => "admin",
+    "manager_password" => "adm123",
+    "logfile" => "/var/www/html/asteriskreport/db/log.log",
+    "monitor" => "/var/spool/asterisk/monitor/",   
+    "users" => array('admin' => 'password', 'user' => 'user')
+);
+
+
