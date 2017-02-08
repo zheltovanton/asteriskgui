425af26e13379e3f07df72eb4a87cc94d2b2b50c
zheltovanton <anton.zheltov@gmail.com>
2017-02-08 10:16:24 +0300

    add login feature

--- index.php
+++ index.php
@@ -1,6 +1,104 @@
 <?php
 
+/**
+ * Asterisk GUI
+ *
+ * This is the Javascript / HTML / CSS / Ajax related to asterisk app. 
+ * Only for viewing settings and conditions, not to configure. 
+ * Screenshot from real system with Elastix in folder "screenshot". *
+ * Using AMI for request info about channels and mysql for CDR stat. 
+ * 
+ * @author 	Zheltov Anton (anton.zheltov@gmail.com)
+ * @license 	☺ License GNU3
+ *
+ */
+
 $page = '';
+
+require_once 'db/config.php';
+
+$config = include("db/config.php");
+
+session_start(); //Запускаем сессии
+
+class AuthClass {
+    private $_login = "demo"; //Устанавливаем логин
+    private $_password = "demo"; //Устанавливаем пароль
+
+    public function isAuth() {
+        if (isset($_SESSION["is_auth"])) { //Если сессия существует
+            return $_SESSION["is_auth"]; //Возвращаем значение переменной сессии is_auth (хранит true если авторизован, false если не авторизован)
+        }
+        else return false; //Пользователь не авторизован, т.к. переменная is_auth не создана
+    }
+    
+    public function auth($login, $passwors) {
+        if ($login == $this->_login && $passwors == $this->_password) { //Если логин и пароль введены правильно
+            $_SESSION["is_auth"] = true; //Делаем пользователя авторизованным
+            $_SESSION["login"] = $login; //Записываем в сессию логин пользователя
+            return true;
+        }
+        else { //Логин и пароль не подошел
+            $_SESSION["is_auth"] = false;
+            return false; 
+        }
+    }
+    
+    public function getLogin() {
+        if ($this->isAuth()) { //Если пользователь авторизован
+            return $_SESSION["login"]; //Возвращаем логин, который записан в сессию
+        }
+    }
+    
+    
+    public function out() {
+        $_SESSION = array(); //Очищаем сессию
+        session_destroy(); //Уничтожаем
+    }
+}
+
+$auth = new AuthClass();
+
+if (isset($_POST["login"]) && isset($_POST["password"])) { //Если логин и пароль были отправлены
+    if (!$auth->auth($_POST["login"], $_POST["password"])) { //Если логин и пароль введен не правильно
+        echo "<h2 style=\"color:red;\">Wrong auth data!</h2>";
+    }
+}
+
+if (isset($_GET["is_exit"])) { //Если нажата кнопка выхода
+    if ($_GET["is_exit"] == 1) {
+        $auth->out(); //Выходим
+        header("Location: ?is_exit=0"); //Редирект после выхода
+    }
+}
+
+if (!$auth->isAuth()) {   
+    die( '<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="public/css/login.css" />
+</head>
+<body>
+
+  <form method="post" class="modal-content animate" action="">
+    <div class="container">
+      <label><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="login" required>
+
+      <label><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="password" required>
+        
+      <button type="submit">Login</button>
+    </div>
+
+  </form>
+</div>
+
+
+</body>
+</html>');
+ } 
+
 
 
 if (empty($_GET["p"])) {     // if some page requested, then show it, else show page CDR
@@ -44,7 +142,8 @@ echo '<!DOCTYPE html>
 		<li><a href="index.php?p=sip.channelstats" >Channel Stats</a></li>
 		</ul>
 	    </li>
-
+            <li><a href="/?is_exit=1">Logout</a></li> 
+	    </li>
         </ul>
     </div>
     <div class="index-frame">
